1f2e4aa49f82423ef3442793a23210fcb3b527fc
an1221na <58279595+an1221na@users.noreply.github.com>
2020-01-09 12:27:30 +0800

    I finished dashboard (#6)
    * I finished dashboard
    
    * I fixed it.
    
    * I fixed font-size

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+  def user_image(user, classes)
+    @image = user.image
+    if @image.present?
+      @image_user = image_tag(@image.url, class: classes)
+    else
+      @image_user = image_tag("default.jpeg", class: classes)
+    end
+  end
 end
