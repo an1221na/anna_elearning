039e516bfc402c4810857deba73f1c38c547c0d7
尾崎杏奈 <ozakianna@46.0.40.120.broad.fz.fj.dynamic.163data.com.cn>
2020-01-10 12:26:11 +0800

    I fixed indents and added seeds

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,17 +2,17 @@ class ApplicationController < ActionController::Base
     include SessionsHelper
 
     def signed_in_user
-        unless signed_in?
-            flash[:danger] = "Please login first."
-            redirect_to login_url
-        end
+      unless signed_in?
+        flash[:danger] = "Please login first."
+        redirect_to login_url
+      end
     end
 
     def admin_user
-        unless current_user.admin?
-            flash[:danger] = "You're not authorized..."
-            redirect_to root_url
-        end
+      unless current_user.admin?
+        flash[:danger] = "You're not authorized..."
+        redirect_to root_url
+      end
     end
 
 end
