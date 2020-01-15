54c34a7984222807a7b0fb9056151283aa5f3674
尾崎杏奈 <ozakianna@ozakiannanoMacBook-Pro.local>
2020-01-15 11:25:21 +0800

    I added link to back

--- app/models/word.rb
+++ app/models/word.rb
@@ -2,4 +2,8 @@ class Word < ApplicationRecord
   belongs_to :category
   has_many :choices, dependent: :destroy
   accepts_nested_attributes_for :choices, allow_destroy: true
+
+  def correct_answer
+    choices.find_by(correct: true).content
+  end
 end
