004a4430f912f853373aa677527a54318767b7f8
尾崎杏奈 <ozakianna@ozakiannanoMacBook-Pro.local>
2020-01-16 10:12:14 +0800

    I finished lesson index page

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,8 @@ class Category < ApplicationRecord
   validates :description, presence: true
 
   has_many :words, dependent: :destroy
+
+  def number_of_words
+    Word.where(category_id: id)
+  end
 end
