3c5b5d94dc935904f65b57a2d236c991f954e626
尾崎杏奈 <ozakianna@ozakiannanoMacBook-Pro.local>
2020-01-16 11:01:19 +0800

    I added pagination

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_14_215705) do
-
-  create_table "answers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "word_id"
-    t.integer "lesson_id"
-    t.integer "choice_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 2020_01_14_215424) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
